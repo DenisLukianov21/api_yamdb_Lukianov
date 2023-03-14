d1243855d2f281d9a22eb59982b151b0abe5ed12
Denis Lukianov <DenisLukianov2001@yandex.ru>
2023-03-14 22:08:07 +0300

    flake8

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from pathlib import Path
 
 load_dotenv()
-env_path = Path('.')/'infra/.env'
+env_path = Path('.') / 'infra/.env'
 load_dotenv(dotenv_path=env_path)
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
